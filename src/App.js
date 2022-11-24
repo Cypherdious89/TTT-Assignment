21448aff33d445c942b58bce4541a4dd6c8fea50
Priyansh Singh <priyanshsingh1@gmail.com>
2022-11-25 03:54:14 +0530

    Created Final Word Array

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import './App.css';
+import {
+    useState
+} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Click Submit to fetch data.</p>
-        <button>Submit</button>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState("");
+    // const [freq, setFreq]=useState({});
+    const fetchData = async () => {
+        await fetch('https://www.terriblytinytales.com/test.txt')
+            .then(response => {return response.text()})
+            .then(d => {setData(d)})
+            let arr = data.split('\n').filter(e=>e!=="");
+            let ans=[];
+            // console.log(arr);
+            for(let i=0 ; i<arr.length; i++){
+                let temp=arr[i].split(' ');
+                
+                for(let j=0; j<temp.length; j++){
+                    if(temp[j].includes('.com') || temp[j].includes('@') || temp[j].includes('http')){
+                        temp[j] = "LINK";
+                    }
+                }
+
+                for(let j=0 ; j<temp.length; j++){
+                    temp[j] = temp[j].replace(/[^\w\s\\']|_/g, "").replace(/\s+/g, " ");
+                    ans.push(temp[j]);
+                    // console.log(temp[j]);
+                }
+
+            }
+            ans = ans.filter(e=> e !== "");
+            console.log(ans);
+    }
+
+    return ( 
+        <div className = "App" >
+            <header className = "App-header" >
+                <h1 > Click submit to fetch data! </h1>
+                <button className = "submit-btn"onClick = {() => fetchData()} > Submit </button> 
+            </header>
+        </div>
+    );
 }
 
 export default App;
