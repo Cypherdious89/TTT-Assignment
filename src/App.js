e8a638ca3e4a23732f036885cdd60747ea1acce4
Priyansh Singh <priyanshsingh1@gmail.com>
2022-11-24 20:49:37 +0530

    Added Starter React Code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Click Submit to fetch data.</p>
+        <button>Submit</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
